5b323db7d3e4bceab931c482f3e20944f5ac8530
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-01-06 10:13:05 -0500

    Added PID Drive Subsystem Code
     - Added PID Drive Subsystem code for a 2 cim bot

--- DeepSpace2019/src/main/java/frc/robot/Constants.java
+++ DeepSpace2019/src/main/java/frc/robot/Constants.java
@@ -7,4 +7,9 @@ package frc.robot;
 
 public class Constants {
 
+    // Drive Subsystem PID Constants
+    public static final double kP = 0.03;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+
 }
--- DeepSpace2019/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ DeepSpace2019/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,37 +1,36 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import frc.robot.RobotMap;
+import frc.robot.Constants;
+
 /**
  * Add your docs here.
  */
 public class DriveSubsystem extends PIDSubsystem {
-  /**
-   * Add your docs here.
-   */
+
+  // Drivetrain TalonSRX map values (roboRIO port values) - 2 cim
+  TalonSRX Left1 = new TalonSRX(RobotMap.Left1);
+  TalonSRX Left2 = new TalonSRX(RobotMap.Left2);
+  TalonSRX Right1 = new TalonSRX(RobotMap.Right1);
+  TalonSRX Right2 = new TalonSRX(RobotMap.Right2);
+
   public DriveSubsystem() {
-    // Intert a subsystem name and PID values here
-    super("SubsystemName", 1, 2, 3);
-    // Use these to get going:
-    // setSetpoint() - Sets where the PID controller should move the system
-    // to
-    // enable() - Enables the PID controller.
+    // Calls parent constructor of PIDSubsystem with the parameters: "SubsystemName", kP, kI, kD
+    super("DriveSubsystem", Constants.kP, Constants.kI, Constants.kD);
   }
 
-  @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    // Initialize the Talons so that the Left2 and Right2 Talons will follow the movements of the Left1 and Right 1 Talons
+    Left2.set(ControlMode.Follower, RobotMap.Left1);
+    Right2.set(ControlMode.Follower, RobotMap.Right1);
   }
 
-  @Override
   protected double returnPIDInput() {
     // Return your input value for the PID loop
     // e.g. a sensor, like a potentiometer:
@@ -39,9 +38,22 @@ public class DriveSubsystem extends PIDSubsystem {
     return 0.0;
   }
 
-  @Override
   protected void usePIDOutput(double output) {
     // Use output to drive your system, like a motor
     // e.g. yourMotor.set(output);
   }
+
+  // Basic Tank Drive Method
+  public void TankDrive(double left, double right){
+		Left1.set(ControlMode.PercentOutput, left);
+		Left2.set(ControlMode.PercentOutput, left);
+		Right1.set(ControlMode.PercentOutput, right);
+		Right2.set(ControlMode.PercentOutput, right);
+  }
+  
+  // Basic Arcade Drive Method
+  public void ArcadeDrive(double speed, double turn){
+		TankDrive(-speed -turn, speed - turn);
+	}
+
 }
